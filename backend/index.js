80b2ab0eb56f887f39d6d637089369ed11514c63
rokong <167447208+rokong@users.noreply.github.com>
2025-12-18 11:49:11 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,13 @@ dotenv.config();
 const app = express();
 
 // -------------------- MIDDLEWARE --------------------
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://trust-marcket.vercel.app"
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
