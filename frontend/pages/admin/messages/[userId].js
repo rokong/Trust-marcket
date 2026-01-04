ecb1a877777c7020b852470639fda19af63b4343
rokong <167447208+rokong@users.noreply.github.com>
2026-01-04 12:17:19 +0600

    Update [userId].js

--- frontend/pages/admin/messages/[userId].js
+++ frontend/pages/admin/messages/[userId].js
@@ -158,7 +158,7 @@ export default function ChatPage() {
                     </div>
                   )}
                   <button
-                    onClick={() => router.push(`/post/${m.postId}`)}
+                    onClick={() => router.push(`/admin/all-users/${m.postId}`)}
                     className="text-xs text-blue-600 underline"
                   >
                     View Post
