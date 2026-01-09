89ef8f1ce3bd61d08303e136c6aeca9761ded0b7
rokong <167447208+rokong@users.noreply.github.com>
2026-01-09 20:13:06 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "trust-marcket-git-main-nazmuls-projects-dd47be01.vercel.app",
+    "trust-marcket.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
