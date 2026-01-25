1a9b468e0407bd4c78ff9e8cd59e52df6d7174bd
rokong <167447208+rokong@users.noreply.github.com>
2026-01-25 14:22:34 +0600

    Update register.js

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -70,7 +70,7 @@ export default function Register() {
               onChange={(e) => setPassword(e.target.value)}
               required
               className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-              placeholder="Enter your password"
+              placeholder="New password"
             />
           </div>
 
